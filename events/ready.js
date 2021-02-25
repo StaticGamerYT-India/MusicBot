8934d43b5e4eb7375610d31698bc6842c9c0c331
StaticGamerYT-India <77852984+StaticGamerYT-India@users.noreply.github.com>
2021-02-25 21:08:49 +0530

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,7 @@
+module.exports = async (client) => {
+  console.log(`[API] Logged in as ${client.user.username}`);
+  await client.user.setActivity("Music You Noobs", {
+    type: "STREAMING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+    url: "https://www.twitch.tv/nocopyrightsounds"
+  });
+};
